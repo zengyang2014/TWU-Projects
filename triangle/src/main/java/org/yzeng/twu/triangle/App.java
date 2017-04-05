2e85bac5b437901540405aa4d21c401faa30e621
yzeng <yzeng@thoughtworks.com>
2017-04-05 11:04:23 +0800

    George | #0001 | Add test to make sure app can print a asterisk and wrote complement to pass the test

---
+++ triangle/src/main/java/org/yzeng/twu/triangle/App.java
@@ -0,0 +1,18 @@
+package org.yzeng.twu.triangle;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        App app = new App();
+        System.out.println(app.printOneAsterisk());
+    }
+
+    public String printOneAsterisk() {
+        return "*";
+    }
+}
