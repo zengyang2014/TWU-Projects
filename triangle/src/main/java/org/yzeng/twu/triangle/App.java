e3687d04dbe7e5bf4d24ce212340e4ce80ece1f3
yzeng <yzeng@thoughtworks.com>
2017-04-05 12:31:13 +0800

    George | #0002 | Add test to make sure app can draw a vertical asterisk line and wrote complement to pass the test

--- triangle/src/main/java/org/yzeng/twu/triangle/App.java
+++ triangle/src/main/java/org/yzeng/twu/triangle/App.java
@@ -9,7 +9,8 @@ public class App
     public static void main( String[] args )
     {
         App app = new App();
-        System.out.println(app.printOneAsterisk());
+        System.out.println(app.drawHorizontalLine(8));
+        System.out.println(app.drawVerticalLine(8));
     }
 
     public String printOneAsterisk() {
@@ -21,6 +22,15 @@ public class App
         for(int i = 0; i < givenNum; i++) {
             asterisks += "*";
         }
+        return asterisks;
+    }
+
+    public String drawVerticalLine(Integer givenNum) {
+        String asterisks = "";
+        for(int i = 0; i < givenNum; i++) {
+            asterisks +="*\n";
+        }
+
         return asterisks;
     }
 }
--- triangle/src/test/java/org/yzeng/twu/triangle/AppTest.java
+++ triangle/src/test/java/org/yzeng/twu/triangle/AppTest.java
@@ -7,43 +7,42 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
+public class AppTest
+        extends TestCase {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
-        super( testName );
+    public AppTest(String testName) {
+        super(testName);
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
     }
 
     /**
      * Rigourous Test :-)
      */
     protected App app;
+
     protected void setUp() {
         app = new App();
     }
-    public void testApp()
-    {
-        assertTrue( true );
+
+    public void testApp() {
+        assertTrue(true);
     }
+
     public void testPrintOneAsterisk() {
         String given = "*";
         String when = app.printOneAsterisk();
 
-        assertEquals(given,when);
+        assertEquals(given, when);
     }
 
     public void testDrawHorizontalLine() {
@@ -51,5 +50,12 @@ public class AppTest
         String output = app.drawHorizontalLine(givenNum);
 
         assertTrue(output.equals("********"));
-}
+    }
+
+    public void testDrawVerticalLine() {
+        Integer givenNum = 3;
+        String output = app.drawVerticalLine(givenNum);
+
+        assertTrue(output.equals("*\n*\n*\n"));
+    }
 }
